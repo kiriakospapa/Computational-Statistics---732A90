40fa1d964cd0ac54d346b2b93bf30e65f4f454e8
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-11-14 22:32:50 +0200

    Finished the code of second question, still need to make comments and explain

--- lab2.R
+++ lab2.R
@@ -26,9 +26,27 @@ loglikelihood <- function(params, data){
   return(-loglikelihood)
 }
 
+gradient <- function(params, data){
+  
+  n <- length(data)
+  mean_value <- (sum(x) - 2 * n * params[1]) / (params[2] ^ 2)
+  sigma_squared <- (-n/params[2]) + (1/params[3]^3) * (sum(data - params[2]))^2
+  print(mean_value)
+  
+  return(c(mean_value,sigma_squared))
+}
 
 result1 = optim(c(0, 1), fn=loglikelihood, data=data, method = "BFGS") 
 result1$par
 
 result2 = optim(c(0, 1), fn=loglikelihood, data=data, method = "CG") 
 result2$par
+
+
+result3 = optim(c(0, 1), fn=loglikelihood, data=data, method = "BFGS", gr = gradient) 
+result3$par
+
+result4 = optim(c(0, 1), fn=loglikelihood, data=data, method = "CG", gr = gradient) 
+result4$par
+
+
