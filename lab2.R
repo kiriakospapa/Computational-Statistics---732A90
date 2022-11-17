7929f3a42349b0fbe5d3865e2179c0129e974d9f
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-11-17 16:26:17 +0200

    added the right one

--- lab2.R
+++ lab2.R
@@ -1,6 +1,51 @@
+# Question 1
 f <- function (x, a0, a1, a2){
   out <- a0 + a1*x + a2*(x^2)
   return(out)
+}
+
+squarred_error <- function(params, data, f1){
+  print(params)
+  y <- 0
+  for(x in data){
+    y = y + (f(x, params[1], params[2], params[3])- f1(x)) ^ 2
+  }
+  return(y)
+}
+
+find_alphas <- function(f, data){
+ alphas <- optim(c(1,1,1), fn=squarred_error, f1=f, data = data)
+ return(alphas)
+}
+
+x <- seq(0,1, by = 0.0001)
+
+tmp <- function(n, f1){
+  x <- seq(0,1, by=0.0001)
+  p <- round(length(x) / n)
+  percent <- round(length(x) / n)
+  for(i in 0:(p-1)){
+    data2 <- x[((i * p) + 1 ): ((i * percent) + percent)]
+    first_point <- data2[1]
+    last_point <- data2[length(data2)]
+    middle_point <- data2[round(length(data2)/2)]
+    print(f1(first_point))
+    print(f1(last_point))
+    print(f1(middle_point))
+    
+    if ((f1(middle_point)> f1(last_point)) || (f1(middle_point)> f1(first_point))){
+      stop("it's wrong")
+    }
+  }
+}
+
+# Question 3
+f1 <- function(x){
+ return(-x*(1-x)) 
+}
+
+f2 <- function(x){
+  return(-x*sin(10*pi*x))
 }
 
 loss_function <- function(x, a){
@@ -8,6 +53,12 @@ loss_function <- function(x, a){
   return()
 }
 
+
+
+
+#======================================
+
+# Question 1
 load("data.RData")
 
 
@@ -49,4 +100,5 @@ result3$par
 result4 = optim(c(0, 1), fn=loglikelihood, data=data, method = "CG", gr = gradient) 
 result4$par
 
+# We have to answer question 4 as well
 
