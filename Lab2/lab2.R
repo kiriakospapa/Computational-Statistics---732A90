fd8124c98f7feaa7986c93ae74615a579a76f4b0
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-11-17 17:59:30 +0200

    Added the folder

---
+++ Lab2/lab2.R
@@ -0,0 +1,135 @@
+# Question 1
+f <- function (x, a0, a1, a2){
+  out <- a0 + a1*x + a2*(x^2)
+  return(out)
+}
+
+squarred_error <- function(params, data, f1){
+  print(params)
+  y <- 0
+  for(x in data){
+    y = y + (f(x, params[1], params[2], params[3])- f1(x)) ^ 2
+  }
+  return(y)
+}
+
+find_alphas <- function(f, data){
+ alphas <- optim(c(1,1,1), fn=squarred_error, f1=f, data = data)
+ return(alphas)
+}
+
+x <- seq(0,1, by = 0.0001)
+
+tmp <- function(n, f1){
+  x <- seq(0,1, by=0.0001)
+  p <- round(length(x) / n)
+  percent <- round(length(x) / n)
+  for(i in 0:(p-1)){
+    data2 <- x[((i * p) + 1 ): ((i * percent) + percent)]
+    first_point <- data2[1]
+    last_point <- data2[length(data2)]
+    middle_point <- data2[round(length(data2)/2)]
+    print(f1(first_point))
+    print(f1(last_point))
+    print(f1(middle_point))
+    
+    if ((f1(middle_point)> f1(last_point)) || (f1(middle_point)> f1(first_point))){
+      stop("it's wrong")
+    }
+  }
+}
+
+# Question 3
+f1 <- function(x){
+ return(-x*(1-x)) 
+}
+
+f2 <- function(x){
+  return(-x*sin(10*pi*x))
+}
+
+loss_function <- function(x, a){
+  out <- f(a)
+  return()
+}
+
+
+
+
+#======================================
+
+# Question 1
+load("data.RData")
+
+
+# Question 2
+n = length(data)
+ll_mean_value = (1/n) * sum(data)
+ll_variance = sqrt((1 / n) * sum((data - mean(data)) ^ 2))
+
+
+# Question 3
+loglikelihood <- function(params, data){
+  mean_value = params[1]
+  sigma_squared = params[2]
+  n = length(data)
+  loglikelihood = -(n / 2) * log( 2 * pi) - (n / 2) * log(abs(sigma_squared)) - ((sum((data - mean_value)^2))/(2*sigma_squared))
+  return(-loglikelihood)
+}
+
+gradient <- function(params, data){
+  # Here we took the partial derivatives from loglikehood with respect to mean and sigma
+  n <- length(data)
+  mean_value <- (sum(x) - 2 * n * params[1]) / (params[2] ^ 2)
+  sigma_squared <- (-n/params[2]) + (1/params[3]^3) * (sum(data - params[2]))^2
+  print(mean_value)
+  
+  return(c(mean_value,sigma_squared))
+}
+
+result1 = optim(c(0, 1), fn=loglikelihood, data=data, method = "BFGS") 
+result1$par
+
+result2 = optim(c(0, 1), fn=loglikelihood, data=data, method = "CG") 
+result2$par
+
+
+result3 = optim(c(0, 1), fn=loglikelihood, data=data, method = "BFGS", gr = gradient) 
+result3$par
+
+result4 = optim(c(0, 1), fn=loglikelihood, data=data, method = "CG", gr = gradient) 
+result4$par
+
+# Question: Why it is a bad idea to maximize likelihood rather than maximizing 
+#log–likelihood
+
+# Answer: Because maximasing the likelihood there is chance to face an overflow 
+# problem. While with the log-likehood the value of likehood will be much smaller
+# as it will be in log.
+
+# We have to answer question 4 as well
+
+optimal_mi= c(result1$par[1], result2$par[1], result3$par[1], result4$par[1])
+optimal_sigma = c(result1$par[2], result2$par[2], result3$par[2], result4$par[2])
+functions_required = c(result1$counts["function"], result2$counts["function"],
+                     result3$counts["function"], result4$counts["function"])
+gradient_evaluations_required = c(result1$counts["gradient"], result2$counts["gradient"],
+                                  result3$counts["gradient"], result4$counts["gradient"])
+converged = c("Yes", "Yes", "Yes", "Yes")
+table = data.frame(optimal_mi, optimal_sigma, functions_required 
+                   ,gradient_evaluations_required, converged)
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
